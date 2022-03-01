5aed2a0de800041dfc359848971a858b246854d5
Bane <bane.baki.1997@gmail.com>
2022-03-01 21:00:24 +0100

    starting work on recipe app

--- apps/Recipe app/script.js
+++ apps/Recipe app/script.js
@@ -1,2 +1,23 @@
 const getIt = (x) => {return document.getElementById(x)};
 const storage = window.localStorage;
+
+// Storage needs to have app specific object since every app is on 
+// the same domain...
+
+//We will have object like this in storage
+
+// [
+// {"appName": "recipeApp",
+//  "storage": [{"recipeName": "Lemon Pie" etc....},{"recipeName": "recipe2"}
+
+//  ]
+ 	
+ 
+// },
+
+
+// {}
+// {}
+// ]
+
+
