fd9b17c2b4fc598c4dd26b27a3aec1987af20ce5
Bane <bane.baki.1997@gmail.com>
2022-03-02 21:37:08 +0100

    working on recipe app

--- apps/Recipe app/script.js
+++ apps/Recipe app/script.js
@@ -1,23 +1,91 @@
 const getIt = (x) => {return document.getElementById(x)};
 const storage = window.localStorage;
-
-// Storage needs to have app specific object since every app is on 
-// the same domain...
-
-//We will have object like this in storage
-
-// [
-// {"appName": "recipeApp",
-//  "storage": [{"recipeName": "Lemon Pie" etc....},{"recipeName": "recipe2"}
-
-//  ]
- 	
- 
-// },
+const storageList = JSON.parse(storage.getItem("recipeApp"));
 
 
-// {}
-// {}
-// ]
+const searchInput = getIt("searchInput");
+const displayRecipes = getIt("recipeList");
+const addButton = getIt("addRecipe");
 
+window.onload = () => {
+	clearInputs();
+	loadItems();
+}
+
+
+const clearInputs = () => {
+	searchInput.value = "";
+}
+
+const loadItems = (items) => {
+
+	clearItemList // so that you don't duplicate
+	if (items === null){
+		// storage.forEach // createElement...
+	} else {
+		// items will be an array of id's that search function will send
+		// so we load storage.forEach if item.id === storage.id
+		// and we createElement
+	}
+
+	}
+
+
+const createElement = (id,name,text, ingredients, check) => {
+
+}
+
+const addHandler = () => {
+	window.location = "Add/index.html";
+
+}
+
+const searchHandler = (e) => {
+	const search = searchInput.value;
+	const idList = [];
+
+	storageList.forEach(x => {
+	if (x.recipeName.includes(search)){
+		console.log(x);
+
+		//.......
+		//.......
+		//.......
+		//This is where you left off
+		//Check to see if this works.....
+		//.......
+		//.......
+		//.......
+
+	} 
+
+
+
+
+
+
+
+	})
+
+
+
+
+
+	loadItems(idList);
+
+
+
+}
+	
+addButton.addEventListener("click", addHandler);
+searchInput.addEventListener("input", searchHandler);
+
+
+
+
+// const createStorage = () => storage.setItem("recipeApp", []);
+// const loadStorage = () =>  {
+// 	if (storage.getItem("recipeApp") != null){
+// 	return storage.getItem("recipeApp")}
+// }
 
