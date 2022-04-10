aa55ebbeee5f8c973add131d81377dcc2c40108d
Bane <bane.baki.1997@gmail.com>
2022-04-10 20:10:45 +0200

    making progress slowly

--- apps/Level 2/Tetris Game/script.js
+++ apps/Level 2/Tetris Game/script.js
@@ -70,12 +70,42 @@ var gameArea =
 			}
 	};
 var ctx = gameArea.canvas.getContext("2d");
+var currentShapeObj;
+var currentRotation;
+var rotationNum;
+var currentTopLeft;
 
+var canvasArray = createCanvasArray();
+
+//Create CanvasArray
+	function createCanvasArray()
+		{
+			var w = 800/20;  //40 
+			var h = 500/20;  //25
+			var array = [];
+			var zeros = [];
+			// adds 40 0's to zeros array;
+			for (x = 0; x<w; x++)
+						{
+							zeros.push(0);
+						}
+
+
+			// for each 25 rows
+			// add 40 zeros
+			for (i = 0;i<h;i++)
+				{
+					// for each row, add 40 zeros
+					array.push(zeros);
+				}
+
+			return array;
+		}
 
 //Shapes and rotations
 	
 	var shapes = [
-				{"num":0;"shapeColor":"red";"rotation":[
+				{"num":1;"shapeColor":"red";"rotation":[
 						[
 							[1,1],
 							[1,1]
@@ -94,70 +124,231 @@ var ctx = gameArea.canvas.getContext("2d");
 						]
 					]
 				},
-				{"num":1;"shapeColor":"grey";"rotation":[
-						[],
-						[],
-						[],
-						[]
+				{"num":2;"shapeColor":"grey";"rotation":[
+						[
+							[0,2],
+							[0,2],
+							[2,2]
+						],
+						[
+							[2,0,0],
+							[2,2,2]
+						],
+						[
+							[2,2],
+							[2,0],
+							[2,0]
+						],
+						[
+							[2,2,2],
+							[0,0,2]
+						]
 					]
 				},
-				{"num":2;"shapeColor":"green";"rotation":[
-						[],
-						[],
-						[],
-						[]
+				{"num":3;"shapeColor":"green";"rotation":[
+						[
+							[3,0],
+							[3,0],
+							[3,3]
+						],
+						[
+							[3,3,3],
+							[3,0,0]
+						],
+						[
+							[3,3],
+							[0,3],
+							[0,3]
+						],
+						[
+							[0,0,3],
+							[3,3,3]
+						]
 					]
 				},
-				{"num":3;"shapeColor":"blue";"rotation":[
-						[],
-						[],
-						[],
-						[]
+				{"num":4;"shapeColor":"blue";"rotation":[
+						[
+							[0,4,0],
+							[4,4,4]
+						],
+						[
+							[4,0],
+							[4,4],
+							[4,0]
+						],
+						[
+							[4,4,4],
+							[0,4,0]
+						],
+						[
+							[0,4],
+							[4,4],
+							[0,4]
+						]
 					]
 				},
-				{"num":4;"shapeColor":"purple";"rotation":[
-						[],
-						[],
-						[],
-						[]
+				{"num":5;"shapeColor":"purple";"rotation":[
+						[
+							[5,5,0],
+							[0,5,5]
+						],
+						[
+							[0,5],
+							[5,5],
+							[5,0]
+						],
+						[
+							[5,5,0],
+							[0,5,5]
+						],
+						[
+							[0,5],
+							[5,5],
+							[5,0]
+						]
 					]
 				},
-				{"num":5;"shapeColor":"yellow";"rotation":[
-						[],
-						[],
-						[],
-						[]
+				{"num":6;"shapeColor":"yellow";"rotation":[
+						[
+							[0,6,6],
+							[6,6,0]
+						],
+						[
+							[6,0],
+							[6,6],
+							[0,6]
+						],
+						[
+							[0,6,6],
+							[6,6,0]
+						],
+						[
+							[6,0],
+							[6,6],
+							[0,6]
+						]
 					]
 				},
-				{"num":6;"shapeColor":"pink";"rotation":[
-						[],
-						[],
-						[],
-						[]
+				{"num":7;"shapeColor":"pink";"rotation":[
+						[
+							[7],
+							[7],
+							[7],
+							[7]
+						],
+						[
+							[7,7,7,7]
+						],
+						[
+							[7],
+							[7],
+							[7],
+							[7]
+						],
+						[
+							[7,7,7,7]
+						]
 					]
+				
 				}
 
 			]
 
-	// var cubeShape = new cubeShapeF();
-	// var leftLShape = new leftLShapeF();
-	// var rightLShape = new rightLShapeF();
-	// var tShape = new tShapeF();
-	// var leftZShape = new leftZShapeF();
-	// var rightZShape = new rightZShapeF();
-	// var lineShape = new lineShapeF();
+
+
+//Shape generation
+	
+	// Generates random shape and sets initial rotation to 0
+	function generateShape()
+		{
+			var random = randomInteger(1,7);
+
+			currentShapeObj = shapes[random];
+			rotationNum = 0;
+			currentRotation = currentShapeObj.rotation[rotationNum];
+		}
+
 
 
 //Shape Placment
+	
+	//places the new shape at the start point
+	function spawnShape()
+		{
+			var rowCount = 0;
+			var columnCount = 0;
+			var middle = 20;
+			currentTopLeft = {"x":20;"y":0};
+												//currentRotation =   [[1,1,1],
+												//					   [1,0,0]]
+
+			currentRotation.forEach(row =>{
+
+				row.forEach(column =>{ 
+					canvasArray[rowCount][middle] = column; //this position = the shape number
+					middle++;
+				})
+			rowCount++;
+			middle = 20;
+			})
+
+
+		}
+
+	//clears the previous rotation/movement
+	function clearPrevious()
+		{
+			var startPosition = canvasArray[currentTopLeft.x][currentTopLeft.y];
+			var rowStart = currentTopLeft.x;
+			var numStart = currentTopLeft.y;
+			var rowCount = 0;
+			var numCount = 0;
+			var shapeNum = currentShapeObj.num;
+
+			//What im trying to do here is simple
+			// compare the shapes side to side and say,
+			// if our block is 0 then we don't touch that block on the canvas, which might not be zero..
+			// that block might be from some other shape, and we don't want to delete that..
+
+			currentRotation.forEach(row =>{
+				row.forEach(num =>{ 
+					if (num === shapeNum)
+						{
+							if (isItZero(rowStart+rowCount,numStart+numCount))
+								{canvasArray[rowStart+rowCount][numStart+numCount] = 0};
+
+						}
+					numCount++;
+				})
+				rowCount++;
+			})
+		}
+
+	//check if 1 or 0
+	function isItZero(x,y)
+		{
+			(canvasArray[x][y] === 0) ? return true : return false;
+		}
 
 
 //Shape Rotation
+
+	//rotates current shape clockwise
+	function rotateShape()
+		{
+
+		}
 
 
 //Shape Movement
 
 
 //Shape Collision
+
+
+//Check/delete row
+
+	//if a row is filled, delete it and move the above shapes down.
 
 
 //Shape Rendering
