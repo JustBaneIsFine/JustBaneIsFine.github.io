109e6b7620bc7b7d380d83449bed98fe2860a590
Bane <bane.baki.1997@gmail.com>
2022-10-24 21:31:01 +0200

    Setting up for testing

--- apps/Level 3/job-hunt-website/Front-end/src/mocks/handlers.ts
+++ apps/Level 3/job-hunt-website/Front-end/src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('path', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json([{ data: 'data' }]));
+  rest.post('/register', (req, res, ctx) => {
+    return res(ctx.status(200, 'you are logged in'));
   }),
 ];
