927b4b6ac7fe839b4509c4487cbc13795d712dc9
Bane <bane.baki.1997@gmail.com>
2022-10-27 20:40:13 +0200

    fixed spelling

--- apps/Level 3/job-hunt-website/Front-end/src/js/register.ts
+++ apps/Level 3/job-hunt-website/Front-end/src/js/register.ts
@@ -4,11 +4,11 @@ import { sendRequest } from './communication';
 export async function submitRegister(username, password) {
   {
     const errors = { usernameError: '', passwordError: '' };
-    const nameTrimed = username.trim();
-    const passTrimed = password.trim();
-    const inputIsValid = validateInput(nameTrimed, passTrimed, errors);
+    const nameTrimmed = username.trim();
+    const passTrimmed = password.trim();
+    const inputIsValid = validateInput(nameTrimmed, passTrimmed, errors);
     if (inputIsValid) {
-      const data = { username: nameTrimed, password: passTrimed };
+      const data = { username: nameTrimmed, password: passTrimmed };
       const result = await sendRequest('/register', 'post', data);
       return result;
     } else {
