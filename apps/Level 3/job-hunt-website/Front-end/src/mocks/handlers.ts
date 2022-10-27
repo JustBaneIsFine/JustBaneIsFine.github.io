2a30319e60f4a258fcda70dd5b5c0eb3dd4cb81c
Bane <bane.baki.1997@gmail.com>
2022-10-27 20:37:28 +0200

    added tests for the login component

--- apps/Level 3/job-hunt-website/Front-end/src/mocks/handlers.ts
+++ apps/Level 3/job-hunt-website/Front-end/src/mocks/handlers.ts
@@ -2,6 +2,9 @@ import { rest } from 'msw';
 
 export const handlers = [
   rest.post('/register', (req, res, ctx) => {
+    return res(ctx.status(200, 'you are registered in'));
+  }),
+  rest.post('/login', (req, res, ctx) => {
     return res(ctx.status(200, 'you are logged in'));
   }),
 ];
--- apps/Level 3/job-hunt-website/Front-end/src/tests/unit/login.test.tsx
+++ apps/Level 3/job-hunt-website/Front-end/src/tests/unit/login.test.tsx
@@ -1,25 +1,132 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import Login from '../../pages/login';
+import * as validate from '../../js/inputValidation';
+import { server } from '../../mocks/server';
+import { rest } from 'msw';
 
+//backend does not interest you, only what we can control here
+//so mock what back-end sends you if there is a need
+afterEach(() => {
+  jest.clearAllMocks();
+  jest.restoreAllMocks();
+});
 describe('Login form works', () => {
-  test('login successful', () => {
-    //hello
-  });
+  describe('front-end login input checks', () => {
+    //
+    test('password is too short', async () => {
+      await inputData('usernameIsGood', '0000');
+      const isThere = await screen.findByPlaceholderText('password is too short');
+      expect(isThere).toBeInTheDocument();
+    });
 
-  test('login failed, Username does not exsist', () => {
-    // const o = 5;
-    //back nd does not interest you, only what we can control here
-    //so mock what back-end sends you if there is a need
+    test('password is too long', async () => {
+      await inputData(
+        'usernameIsGood',
+        '11111111111111111111111111111111111111111111111111111111111',
+      );
+      const isThere = await screen.findByPlaceholderText('password is too long');
+      expect(isThere).toBeInTheDocument();
+    });
+
+    test('username is too short', async () => {
+      await inputData('xx', 'PasswordIsGood');
+      const isThere = await screen.findByPlaceholderText('username is too short');
+      expect(isThere).toBeInTheDocument();
+    });
+
+    test('username is too long', async () => {
+      await inputData(
+        'usernameIsWayTooLong11111111111111111111111111111111111111111111',
+        'passIsGood',
+      );
+      const isThere = await screen.findByPlaceholderText('username is too long');
+      expect(isThere).toBeInTheDocument();
+    });
   });
-  test('login failed, wrong combination username/password', () => {
-    // const o = 5;
+  describe('back-end responds with errors', () => {
+    describe('bad input format', () => {
+      test('username is too short', async () => {
+        server.use(
+          rest.post('/login', (req, res, ctx) => {
+            return res(ctx.status(400, 'username is too short'));
+          }),
+        );
+
+        jest.spyOn(validate, 'validateInput').mockReturnValue(true);
+        await inputData('sh', 'passwordIsGood');
+        const isThere = await screen.findByPlaceholderText('username is too short');
+        expect(isThere).toBeInTheDocument();
+      });
+      test('password is too short', async () => {
+        server.use(
+          rest.post('/login', (req, res, ctx) => {
+            return res(ctx.status(400, 'password is too short'));
+          }),
+        );
+
+        jest.spyOn(validate, 'validateInput').mockReturnValue(true);
+        await inputData('usernameIsGood', '0000');
+        const isThere = await screen.findByPlaceholderText('password is too short');
+        expect(isThere).toBeInTheDocument();
+      });
+      test('username is too long', async () => {
+        server.use(
+          rest.post('/login', (req, res, ctx) => {
+            return res(ctx.status(400, 'username is too long'));
+          }),
+        );
+
+        jest.spyOn(validate, 'validateInput').mockReturnValue(true);
+        await inputData('usernameIsWayTooLong11111111111111111111111111111111', '0000');
+        const isThere = await screen.findByPlaceholderText('username is too long');
+        expect(isThere).toBeInTheDocument();
+      });
+      test('password is too long', async () => {
+        server.use(
+          rest.post('/login', (req, res, ctx) => {
+            return res(ctx.status(400, 'password is too long'));
+          }),
+        );
+
+        jest.spyOn(validate, 'validateInput').mockReturnValue(true);
+        await inputData('usernameIsGood', 'passwordIsWayTooLong111111111111111111111111111');
+        const isThere = await screen.findByPlaceholderText('password is too long');
+        expect(isThere).toBeInTheDocument();
+      });
+    });
+    describe('other errors', () => {
+      //
+      test('wrong username/password combination', async () => {
+        server.use(
+          rest.post('/login', (req, res, ctx) => {
+            return res(ctx.status(400, 'wrong username/password combination'));
+          }),
+        );
+        await inputData('usernameIsGood', 'passwordIsGood');
+        const isThere = await screen.findAllByPlaceholderText(
+          'wrong username/password combination',
+        );
+
+        expect(isThere.length).toBeGreaterThanOrEqual(1);
+      });
+    });
   });
-  test('login failed, no password entered', () => {
-    // const o = 5;
+  test('login successful', async () => {
+    //
   });
 });
 
-describe('Login form ui??', () => {
-  // seperate UI testing?
-});
+async function inputData(name, pass) {
+  user.setup();
+  render(<Login />);
+  const inputName = screen.getByTestId('inputUser');
+  const inputPass = screen.getByTestId('inputPass');
+  const submitButton = screen.getByRole('button', { name: /click here to login/i });
+  await user.click(inputName);
+  await user.paste(name);
+  await user.click(inputPass);
+  await user.paste(pass);
+  await user.click(submitButton);
+}
