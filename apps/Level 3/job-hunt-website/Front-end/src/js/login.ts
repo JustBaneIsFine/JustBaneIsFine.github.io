30ae0db2b57edc9efef3c77b85fc6180873f6862
Bane <bane.baki.1997@gmail.com>
2022-10-27 20:39:46 +0200

    added a function for handling login data

--- apps/Level 3/job-hunt-website/Front-end/src/js/login.ts
+++ apps/Level 3/job-hunt-website/Front-end/src/js/login.ts
@@ -1,4 +1,18 @@
-export function submitLogin(username, password) {
-  //hash password and send to server
-  //server returns checked or not checked
+import { validateInput } from './inputValidation';
+import { sendRequest } from './communication';
+
+export async function submitLogin(username, password) {
+  {
+    const errors = { usernameError: '', passwordError: '' };
+    const nameTrimmed = username.trim();
+    const passTrimmed = password.trim();
+    const inputIsValid = validateInput(nameTrimmed, passTrimmed, errors);
+    if (inputIsValid) {
+      const data = { username: nameTrimmed, password: passTrimmed };
+      const result = await sendRequest('/login', 'post', data);
+      return result;
+    } else {
+      return errors;
+    }
+  }
 }
