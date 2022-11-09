23a713ea1d741518bac5f20aa971ee88c345e350
Bane <bane.baki.1997@gmail.com>
2022-11-09 19:41:42 +0100

    deleting unnecessary files

--- apps/Level 3/job-hunt-website/Back-end/server.test.ts
+++
@@ -1,9 +0,0 @@
-import { testFunc } from './server';
-
-test('should be 7', () => {
-    expect(testFunc(2, 5)).toBe(7);
-});
-
-test('should be 22', () => {
-    expect(testFunc(21, 1)).toBe(22);
-});
